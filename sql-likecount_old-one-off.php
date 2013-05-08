f3792effd244affdeac667bd30e9155cc9ce40ae
Katie Patrick <katie.patrick@thegreenpages.com.au>
2013-05-09 08:28:07 +1000

    Single loop and query to add likecount_old to database to get started on upvote ranking

---
+++ sql-likecount_old-one-off.php
@@ -0,0 +1,25 @@
+<?php
+
+	mysql_connect("127.0.0.1", "s1-wordpress", "7BXmxPmwy4LJZNhR") or die(mysql_error());
+	mysql_select_db("s1-wordpress") or die(mysql_error());
+	
+	$sql = "SELECT DISTINCT post_ID
+       		FROM wp_postmeta";
+       		
+       		
+	$db_result = mysql_query($sql);
+
+	$data_set = mysql_num_rows($db_result);
+	
+	$i = 0;
+	while ($i < $data_set) { 
+
+		mysql_data_seek($db_result, $i);
+		$row = mysql_fetch_object($db_result);
+		$post_ID = $row->post_ID;
+	
+		mysql_query('INSERT INTO wp_postmeta SET meta_key="likecount_old", meta_value="1", post_ID = "' . $post_ID . '"');
+		$i++;		
+	}
+//. $post_ID .
+?>
