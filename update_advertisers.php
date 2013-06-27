c1f5f9cda6b75c0cb605a685e178a53182639102
Katie Patrick <katie.patrick@thegreenpages.com.au>
2013-06-28 07:22:15 +1000

    unix times for test advertisers

--- update_advertisers.php
+++ update_advertisers.php
@@ -5,8 +5,8 @@
 		// reg_advertiser -> 1
 		// advertiser_status -> active, used_up, paused
 		
-		// meta_key = 'reg_advertiser' meta_value = 1
-		// ('reg_advertiser'->1)
+		meta_key = 'reg_advertiser' meta_value = 1
+		('reg_advertiser'->1)
 		
 		mysql_query('INSERT INTO wp_usermeta 
 					SET meta_key="advertiser_status", meta_value="active", user_ID = "' . $user_ID . '"
@@ -28,27 +28,30 @@
 // meta_key = charigfy_ID 		-> 	meta_value = number;
 // Meta_key = chargify_product_ID 	-> 	meta_value = number;
 
-// Biome 				wp=259	 	ch=1733583 	sub=1758911
-// New Leaf Corporate 	wp=1505  	ch=1787860 	sub=1815521
-// 15 Trees 			wp=1936		ch=2045804 	sub=2081871
-// Climate Friendly  	wp=1551  	ch=1828803 	sub=1859829
+// Biome 				wp=259	 	ch=1733583 	sub=1758911 signup=05/03/2012 #unix=1336003200
+// New Leaf Corporate 	wp=1505  	ch=1787860 	sub=1815521 signup=05/18/2012 #unix=1337299200
+// 15 Trees 			wp=1936		ch=2045804 	sub=2081871 signup=07/28/2012 #unix=1343433600
+// Climate Friendly  	wp=1551  	ch=1828803 	sub=1859829 signup=05/27/2012 #unix=1338076800
 
 
 		
 'INSERT INTO wp_usermeta 
-SET meta_key="budget", meta_value="active", user_id = "2"';
+SET meta_key="budget_status", meta_value="active", user_id = "2"'
 
 'INSERT INTO wp_usermeta					 
-SET meta_key="productID", meta_value="27023", user_id = "2"';
+SET meta_key="productID", meta_value="27023", user_id = "2"'
 					
 'INSERT INTO wp_usermeta					 
-SET meta_key="chargifyID", meta_value="27023", user_id = "2"';
+SET meta_key="chargifyID", meta_value="27023", user_id = "2"'	
 
 'INSERT INTO wp_usermeta					 
-SET meta_key="subscriptionID", meta_value="27023", user_id = "2"';			
+SET meta_key="subscriptionID", meta_value="27023", user_id = "2"'
+
+'INSERT INTO wp_usermeta					 
+SET meta_key="adv_signup_time", meta_value="27023", user_id = "2"'			
 
 'INSERT INTO wp_usermeta 
 SET meta_key="advertiser_status", meta_value="active"
-WHERE meta_value = "reg_advertiser"';
+WHERE meta_value = "reg_advertiser"'
 
 ?>
