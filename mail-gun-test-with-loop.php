62cb57ca9b2b48f9de3f9f6f81939bb5cc277286
jesse-browne <jb@greenpag.es>
2013-09-15 21:57:02 -0700

    Still more dubugging messages form featured images

--- mail-gun-test-with-loop.php
+++ mail-gun-test-with-loop.php
@@ -77,7 +77,6 @@ function get_post_image($row) {
 	    }
 	} elseif ( empty($image_url) && ($row->_thumbnail_id != NULL) && ($row->post_type != 'gp_news') ) {
 	    // Get url for featured image thumbnail from db
-	    
 	    echo PHP_EOL;
 	    echo 'Get url for featured image thumbnail from db';
 	    echo PHP_EOL;
@@ -95,9 +94,9 @@ function get_post_image($row) {
         	    if ($post_date_tr == $new_post_date_tr) {
             	        $file_type =  substr($new_row->guid, -4);
             		$len =        strlen($new_row->guid);
-            		$f_name =     substr($new_row->guid, 0, $len - 4);
-            		$s_f_name =   $f_name . '-110x110' . $file_type;
-            		$image_url_img = 'img src="' . $s_f_name .'"';
+            		$f_i_name =     substr($new_row->guid, 0, $len - 4);
+            		$s_f_i_name =   $f_i_name . '-110x110' . $file_type;
+            		$image_url_img = 'img src="' . $s_f_i_name .'"';
         	    }
         	    $i++;
                 }
